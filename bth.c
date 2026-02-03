a11217dd4d88d7459fd54502dfe4967fc3481cbe
MrR736 <mrr6451@outlook.com>
2026-02-03 22:15:26 +0200

    Add files via upload

--- bth.c
+++ bth.c
@@ -1,4 +1,5 @@
-/* bth.c - Bytes To Header Utility
+/**
+ * bth.c - Bytes To Header Utility
  *
  * Copyright (C) 2025 MrR736 <MrR736@users.github.com>
  *
@@ -19,6 +20,7 @@
  * can be found in /usr/share/common-licenses/GPL-3 file.
  */
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,230 +31,246 @@
 
 #include "bth.h"
 
-void print_usage(const char *progname) {
-    vprint("Usage: %s -i <input> -o <output> -f <name> [-c <count>] [-v] [-s] [-t] [-m, --ms-sys]\n\n"
-            "<Options>\n"
-            "  --help, -h       Display this help and exit\n"
-            "  --version, -V    Display version information and exit\n"
-            "  -i <input>       Input file\n"
-            "  -o <output>      Output file\n"
-            "  -f <name>        Name of generated array\n"
-            "  -c <count>       Number of bytes to read (Max: %s)\n"
-            "  -v               Verbose output\n"
-            "  -s               Silent mode\n"
-            "  -t               Text mode\n"
-            "  -m, --ms-sys     MS-SYS mode\n"
-            "  -e               Tests mode\n"
-            "  -h               Show this help message", progname, TEXT_MAX);
+static void print_usage(const char *progname) {
+	LOGI("Usage: %s -i <input> -o <output> -f <name> [-c <count>] [Options]\n\n"
+		"<Commands>\n"
+		"  --help, -h       Display this help and exit\n"
+		"  --version, -V    Display version information and exit\n"
+		"  -i <input>       Input file\n"
+		"  -o <output>      Output file\n"
+		"  -f <name>        Name of generated array\n"
+		"  -c <count>       Number of bytes to read (Max: %s)\n\n"
+		"<Options>\n"
+		"  -v               Verbose output\n"
+		"  -s               Silent mode\n"
+		"  -t               Text mode\n"
+		"  -m, --ms-sys     MS-SYS mode\n"
+		"  -e               Tests mode\n"
+		"  -h               Show this help message", progname, TEXT_MAX);
 }
 
-void print_version(const char *progname) {
-    vprint("%s (Bytes To Header) %s\n\nWritten by MrR736.", progname, VERSION);
+static void print_version(const char *progname) {
+	LOGI("%s (Bytes To Header) %s\n\nWritten by MrR736.", progname, VERSION);
 }
 
-char *sanitize_name(const char *name) {
-    if (!name || !*name) return NULL;
-    char *sanitized = strdup(name);
-    if (!sanitized) return NULL;
-    if (!isalpha(sanitized[0]) && sanitized[0] != '_') sanitized[0] = '_';
-    for (char *c = sanitized + 1; *c; c++) {
-        if (!isalnum(*c) && *c != '_') *c = '_';
-    }
-    return sanitized;
+static char *sanitize_name(const char *name) {
+	if (!name || !*name) return NULL;
+	char *sanitized = strdup(name);
+	if (!sanitized) return NULL;
+	if (!isalpha(sanitized[0]) && sanitized[0] != '_') sanitized[0] = '_';
+	for (char *c = sanitized + 1; *c; c++) { if (!isalnum(*c) && *c != '_') *c = '_'; }
+	return sanitized;
 }
 
-char *uppercase_name(const char *name) {
-    size_t len = strlen(name);
-    char *upper = malloc(len + 1);
-    if (!upper) return NULL;
-    for (size_t i = 0; i < len; i++) upper[i] = toupper((unsigned char)name[i]);
-    upper[len] = '\0';
-    return upper;
+static char *uppercase_name(const char *name) {
+	size_t len = strlen(name);
+	char *upper = malloc(len + 1);
+	if (!upper) return NULL;
+	for (size_t i = 0; i < len; i++) upper[i] = toupper((unsigned char)name[i]);
+	upper[len] = '\0';
+	return upper;
 }
 
-const char* basename(const char *path) {
-    if (!path || !*path) return "unknown";
-    const char *base = strrchr(path, '/');
+static const char* xbasename(const char *path) {
+	if (!path || !*path) return "unknown";
+	const char *base = strrchr(path, '/');
 #ifdef _WIN32
-    const char *win_base = strrchr(path, '\\');
-    if (win_base && (!base || win_base > base)) base = win_base;
+	const char *win_base = strrchr(path, '\\');
+	if (win_base && (!base || win_base > base)) base = win_base;
 #endif
-    return base ? base + 1 : path;
+	return base ? base + 1 : path;
+}
+
+static size_t fpsize(FILE *fp) {
+	if (!fp) {
+		errno = EINVAL;
+		return (size_t)-1;
+	}
+
+	long original_pos = ftell(fp);
+	if (original_pos < 0)
+		return (size_t)-1;
+
+	if (fseek(fp, 0, SEEK_END) != 0)
+		return (size_t)-1;
+
+	long end_pos = ftell(fp);
+	if (end_pos < 0) {
+		fseek(fp, original_pos, SEEK_SET);
+		return (size_t)-1;
+	}
+
+	if (fseek(fp, original_pos, SEEK_SET) != 0)
+		return (size_t)-1;
+
+	if (end_pos < original_pos) {
+		errno = EIO;
+		return (size_t)-1;
+	}
+
+	long diff = end_pos - original_pos;
+	if (diff < 0 || (unsigned long)diff > SIZE_MAX) {
+		errno = EOVERFLOW;
+		return (size_t)-1;
+	}
+
+	return (size_t)diff;
 }
 
 int main(int argc, char *argv[]) {
-    char *input_file = NULL, *output_file = NULL, *function_name = NULL;
-    int count = -1;
-    bool verbose = false, struct_mode = false, text_mode = false, ms_sys_mode = false, test_mode = false;
+	char *input_file = NULL, *output_file = NULL, *function_name = NULL;
+	size_t read_bytes, count = -1;
+	bool verbose = false, struct_mode = false, text_mode = false, ms_sys_mode = false, test_mode = false;
 
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
-            print_usage(argv[0]);
-            return EXIT_SUCCESS;
-        } else if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-V") == 0) {
-            print_version(argv[0]);
-            return EXIT_SUCCESS;
-        } else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc) {
-            count = atoi(argv[++i]);
-            if (count <= 0) {
-                bdebug("Invalid count value");
-                return EXIT_FAILURE;
-            }
-        } else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
-            function_name = argv[++i];
-        } else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
-            input_file = argv[++i];
-        } else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
-            output_file = argv[++i];
-        } else if (strcmp(argv[i], "-v") == 0) {
-            verbose = true;
-        } else if (strcmp(argv[i], "-s") == 0) {
-            struct_mode = true;
-        } else if (strcmp(argv[i], "-t") == 0) {
-            text_mode = true;
-        } else if (strcmp(argv[i], "-e") == 0) {
-            test_mode = true;
-        } else if (strcmp(argv[i], "--ms-sys") == 0 || strcmp(argv[i], "-m") == 0) {
-            ms_sys_mode = true;
-        } else {
-            bdebug("Unknown option '%s'", argv[i]);
-            return EXIT_FAILURE;
-        }
-    }
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
+			print_usage(argv[0]);
+			return EXIT_SUCCESS;
+		} else if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-V") == 0) {
+			print_version(argv[0]);
+			return EXIT_SUCCESS;
+		} else if (strcmp(argv[i], "-c") == 0 && i + 1 < argc) {
+			count = atoi(argv[++i]);
+			if (count <= 0) {
+				LOGE("Invalid count value");
+				return EXIT_FAILURE;
+			}
+		} else if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) { function_name = argv[++i]; }
+		else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) { input_file = argv[++i]; }
+		else if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) { output_file = argv[++i]; }
+		else if (strcmp(argv[i], "-v") == 0) { verbose = true; }
+		else if (strcmp(argv[i], "-s") == 0) { struct_mode = true; }
+		else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "-e") == 0) { text_mode = true; }
+		else if (strcmp(argv[i], "--ms-sys") == 0 || strcmp(argv[i], "-m") == 0) { ms_sys_mode = true; }
+		else {
+			LOGE("Invalid option '%s'\n", argv[i]);
+			return EXIT_FAILURE;
+		}
+	}
+#ifdef ENABLED_BUFFER_MAX
+	if (count > BUFFER_MAX) {
+		LOGE("Maximum Size is %d", BUFFER_MAX);
+		return EXIT_FAILURE;
+	}
+#endif
 
-    #ifdef ENABLED_BUFFER_MAX
-    if (count > BUFFER_MAX) {
-        bdebug("Maximum Size is %d", BUFFER_MAX);
-        return EXIT_FAILURE;
-    }
-    #endif
+	if (!input_file) {
+		print_usage(argv[0]);
+		return EXIT_FAILURE;
+	}
 
-    if (!input_file) {
-        bdebug("Missing required options");
-        return EXIT_FAILURE;
-    }
+	if (!output_file) { output_file = "file.h"; }
+	if (!function_name) { function_name = "function"; }
 
-    if (!output_file) {
-        output_file = "file.h";
-    }
+	char *sanitized = sanitize_name(function_name);
+	char *upper = uppercase_name(sanitized);
+	if (!sanitized || !upper) {
+		LOGE("Memory allocation failed");
+		FREE(sanitized);
+		FREE(upper);
+		return EXIT_FAILURE;
+	}
 
-    if (!function_name) {
-        function_name = "function";
-    }
+	FILE *in = (strcmp(input_file, "-") == 0) ? stdin : fopen(input_file, "rb");
+	if (!in) {
+		LOGE("Opening input file '%s': %s", input_file, strerror(errno));
+		FREE(sanitized);
+		FREE(upper);
+		return EXIT_FAILURE;
+	}
 
-    char *sanitized = sanitize_name(function_name);
-    char *upper = uppercase_name(sanitized);
-    if (!sanitized || !upper) {
-        bdebug("Memory allocation failed");
-        if (sanitized) free(sanitized);
-        if (upper) free(upper);
-        return EXIT_FAILURE;
-    }
+	if (count == (size_t)-1) {
+		count = fpsize(in);
+		if (count == (size_t)-1) {
+			LOGE("Determining input file size: %s", strerror(errno));
+			CLOSE_FILE(in);
+			FREE(sanitized);
+			FREE(upper);
+			return EXIT_FAILURE;
+		}
+	}
 
-    FILE *in = (strcmp(input_file, "-") == 0) ? stdin : fopen(input_file, "rb");
-    if (!in) {
-        bdebug("Opening input file '%s': %s", input_file, strerror(errno));
-        if (sanitized) free(sanitized);
-        if (upper) free(upper);
-        return EXIT_FAILURE;
-    }
+	FILE *out = fopen(output_file, "wb");
+	if (!out) {
+		LOGE("opening output file '%s': %s", output_file, strerror(errno));
+		CLOSE_FILE(in);
+		FREE(sanitized);
+		FREE(upper);
+		return EXIT_FAILURE;
+	}
 
-    FILE *out = fopen(output_file, "w");
-    if (!out) {
-        bdebug("opening output file '%s': %s", output_file, strerror(errno));
-        if (in != stdin) fclose(in);
-        if (sanitized) free(sanitized);
-        if (upper) free(upper);
-        return EXIT_FAILURE;
-    }
+	size_t memory_bytes = 0;
+#ifdef ENABLED_BUFFER_MAX
+	unsigned char buffer[BUFFER_MAX];
+#else
+	unsigned char buffer[count];
+#endif
 
-    size_t memory_bytes = 0;
+	while ((read_bytes = fread(buffer, 1, (count > 0 ? (size_t)(count - memory_bytes) : sizeof(buffer)), in)) > 0) {
+		memory_bytes += read_bytes;
+	}
 
-    #ifdef ENABLED_BUFFER_MAX
-    unsigned char buffer[BUFFER_MAX];
-    #else
-    unsigned char buffer[count];
-    #endif
+	fprintf_t(out,
+		"/*\n"
+		" * Generated with Bytes To Header\n"
+		" * Version: %s\n"
+		" * Input File: %s\n"
+		" * Output File: %s\n"
+		" * Bytes: %zu\n"
+		" */\n\n",VERSION,xbasename(input_file),xbasename(output_file),memory_bytes);
+	if (!ms_sys_mode) {
+		fprintf_t(out, "#ifndef %s_H\n#define %s_H\n\n", upper, upper);
+		fprintf_t(out, "#ifdef __cplusplus\nextern \"C\" {\n#endif\n\n");
+		fprintf_t(out, "#define %s_SIZE %zu\n\n", upper, memory_bytes);
+	}
 
-    size_t read_bytes;
+	rewind(in); // Reset file pointer for second pass.
 
-    while ((read_bytes = fread(buffer, 1, (count > 0 ? (size_t)(count - memory_bytes) : sizeof(buffer)), in)) > 0) {
-        memory_bytes += read_bytes;
-    }
+	if (ms_sys_mode) {
+		fprintf_t(out, "unsigned char %s[] = {", sanitized);
+	} else if (struct_mode) {
+		fprintf_t(out,  "struct %s_struct {\n  unsigned char data[%s_SIZE];\n};\n\n", sanitized, upper);
+		fprintf_t(out, "static const struct %s_struct %s = {\n	.data = {", sanitized, sanitized);
+	} else {
+		fprintf_t(out, "static const unsigned char %s[%s_SIZE] = {", sanitized, upper);
+	}
 
-    fprintf(out,
-    "/*\n"
-    " * Generated with Bytes To Header\n"
-    " * Version: %s\n"
-    " * Input File: %s\n"
-    " * Output File: %s\n"
-    " * Bytes: %zu\n"
-    " */\n\n",
-    VERSION, basename(input_file), basename(output_file), memory_bytes);
-    if (!ms_sys_mode) {
-        fprintf(out, "#ifndef %s_H\n#define %s_H\n\n", upper, upper);
-        fprintf(out, "#ifdef __cplusplus\nextern \"C\" {\n#endif\n\n");
-        fprintf(out, "%s", text_mode ? "" : "#include <stdint.h>\n\n");
-        fprintf(out, "#define %s_SIZE %zu\n\n", upper, memory_bytes);
-    }
+	size_t total_bytes = 0;
+	while ((read_bytes = fread(buffer, 1, (count > 0 ? (size_t)(count - total_bytes) : sizeof(buffer)), in)) > 0) {
+		for (size_t i = 0; i < read_bytes; i++) {
+			if (total_bytes > 0) fprintf_t(out, ",");
+			if (total_bytes % BYTES_PER_LINE == 0) fprintf_t(out, "\n%s", struct_mode ? "		" : "	");
+			fprintf_t(out, "0x%02X", buffer[i]);
+			total_bytes++;
+		}
+	}
 
-    rewind(in); // Reset file pointer for second pass.
+	if (ferror(in)) {
+		LOGE("reading input file");
+		CLOSE_FILE(in);
+		CLOSE_FILE(out);
+		FREE(sanitized);
+		FREE(upper);
+		return EXIT_FAILURE;
+	}
 
-    if (ms_sys_mode) {
-        fprintf(out, "unsigned char %s[%zu] = {", sanitized, memory_bytes);
-    } else if (struct_mode) {
-        fprintf(out, "struct __attribute__((aligned(%s_SIZE))) %s_struct {\n  unsigned char data[%s_SIZE];\n};\n\n", upper, sanitized, upper);
-        fprintf(out, "static const struct %s_struct %s = {\n  .data = {", sanitized, sanitized);
-    } else {
-        fprintf(out, "%s %s[%s_SIZE] = {", text_mode ? "static const uint8_t" : "static const unsigned char", sanitized, upper);
-    }
+	if (struct_mode)
+		fprintf_t(out, "\n	}\n};\n\n");
+	else
+		fprintf_t(out, "\n};\n\n");
 
-    size_t total_bytes = 0;
-    while ((read_bytes = fread(buffer, 1, (count > 0 ? (size_t)(count - total_bytes) : sizeof(buffer)), in)) > 0) {
-        for (size_t i = 0; i < read_bytes; i++) {
-            if (total_bytes > 0) fprintf(out, ", ");
-            if (total_bytes % BYTES_PER_LINE == 0) fprintf(out, "\n%s", struct_mode ? "    " : "  ");
-            fprintf(out, "0x%02x", buffer[i]);
-            total_bytes++;
-        }
-    }
+	if (!ms_sys_mode)
+		fprintf_t(out, "#ifdef __cplusplus\n}\n#endif\n\n#endif // %s_H\n\n", upper);
+	if (test_mode)
+		fprintf_t(out, " /*\n * Tests\n * Bytes: %zu\n */", total_bytes);
 
-    if (ferror(in)) {
-        bdebug("reading input file");
-        fclose(in);
-        fclose(out);
-        free(sanitized);
-        free(upper);
-        return EXIT_FAILURE;
-    }
+	CLOSE_FILE(in);
+	CLOSE_FILE(out);
+	FREE(sanitized);
+	FREE(upper);
 
-    if (struct_mode) {
-        fprintf(out, "\n  }\n};\n\n");
-    } else {
-        fprintf(out, "\n};\n\n");
-    }
+	if (verbose)
+		LOGI("Successfully written %zu bytes to %s", memory_bytes, output_file);
 
-    if (!ms_sys_mode) {
-        fprintf(out, "#ifdef __cplusplus\n}\n#endif\n\n#endif // %s_H\n\n", upper);
-    }
-
-    if (test_mode) {
-        fprintf(out,
-        " /*\n"
-        " * Tests\n"
-        " * Bytes: %zu\n"
-        " */",
-        total_bytes);
-    }
-
-    fclose(in);
-    fclose(out);
-    free(sanitized);
-    free(upper);
-
-    if (verbose) {
-        vprint("Successfully written %zu bytes to %s", memory_bytes, output_file);
-    }
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
--- bth.h
+++ bth.h
@@ -1,18 +1,37 @@
 #ifndef BTH_H
 #define BTH_H
 
-#undef ENABLED_BUFFER_MAX
-
-#define VERSION "1.2-rc4"
-#define BYTES_PER_LINE 12
-
-#ifdef _WIN32
-#define vprint(...) fprintf(stderr, __VA_ARGS__)
-#else
-#define vprint(...) fprintf(stderr, __VA_ARGS__), fputc('\n', stderr)
+#include <stdint.h>
+#ifdef __ANDROID__
+#include <android/log.h>
 #endif
 
-#define bdebug(...) vprint("E: " __VA_ARGS__)
+#undef ENABLED_BUFFER_MAX
+
+#define BYTES_PER_LINE 16
+
+#ifndef VERSION
+#define VERSION "1.2"
+#endif
+
+#ifdef _WIN32
+#define fprintf_t fprintf_s
+#define LOGI(...) fprintf_t(stderr, __VA_ARGS__)
+#elifdef __ANDROID__
+#define fprintf_t fprintf
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "bth", ##__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "bth", ##__VA_ARGS__)
+#else
+#define fprintf_t fprintf
+#define LOGI(...) fprintf_t(stderr, __VA_ARGS__), fputc('\n', stderr)
+#endif
+
+#ifndef __ANDROID__
+#define LOGE(...) LOGI("E: " __VA_ARGS__)
+#endif
+
+#define FREE(x) do { if ((x) != NULL) { free(x); (x) = NULL; } } while(0)
+#define CLOSE_FILE(x) do { if ((x) != NULL) { fclose(x); (x) = NULL; } } while(0)
 
 #ifdef ENABLED_BUFFER_MAX
 #define BUFFER_MAX 4096
